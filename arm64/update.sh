22631c5b391cba15e9674e7fbb690daef88ab628
Chris Rueber <crueber@gmail.com>
2024-03-08 14:50:13 -0600

    Scripts to run and update the whole stack.

---
+++ arm64/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+exclude_dirs=$(cat exclude_dirs)
+include_dirs=$(find . -type d ! -name "*")
+
+exclude_dirs=$(echo "${exclude_dirs[@]}" | sed 's/,/\\,/g') # escape commas to make them special characters in sed command
+for dir in $(find . -type d ! -path "*/\(${exclude_dirs}\)/*" ! -name "*"); do
+  echo "Running docker-compose up -d in $dir..."
+  cd "$dir" && docker-compose pull && docker-compose -f . up -d --build --force-recreate
+done
